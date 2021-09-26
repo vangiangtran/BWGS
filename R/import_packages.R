14d5ce47f7b7d79d32450860f4345ab88a31a2d1
Bangyou Zheng <bangyou.zheng@csiro.au>
2021-09-27 08:31:33 +1000

    Remove header in the file

--- R/import_packages.R
+++ R/import_packages.R
@@ -1,7 +1,3 @@
-# * Author:    Bangyou Zheng (Bangyou.Zheng@csiro.au)
-# * Created:   03:40 PM Saturday, 09 June 2018
-# * Copyright: AS IS
-
 
 #' @importFrom stats anova cor dist lm na.omit predict rnorm runif sd var
 #' @importFrom utils setTxtProgressBar txtProgressBar
